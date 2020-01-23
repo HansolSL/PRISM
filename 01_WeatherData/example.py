ab98da012c69a4e687c273b8a9940859c8004720
HansolShin(Lab) <hansol.shin@snu.ac.kr>
2020-01-23 19:47:03 +0900

    Update SolarDecomposition codes

--- 01_WeatherData/example.py
+++ 01_WeatherData/example.py
@@ -7,15 +7,16 @@ Created on Tue Jan 14 20:23:08 2020
 
 import numpy as np
 from SolarDecomposition import CalSolar
+from Packages import SolarDecomposition as SD
 
 # Global Radiation
-Gl = 4.166667 # W/m2
+Gl = 4.166667  # W/m2
 
 # Time
-Time = [9, 18, 0] # Day of Year, Hour, Minute
+Time = [9, 18, 0]  # Day of Year, Hour, Minute
 
 # Class
-sd = CalSolar()
+sd = SD.CalSolar()
 
 # Decomposition
 Ib, Id, Kt, Sol_Altit, Sol_Azim = sd.Watanabe(Gl, Time)
